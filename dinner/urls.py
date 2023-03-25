9772ba192803139dc39bb95adbf1225eb2b23d8e
Jacob Atkinson <jacobatkinson200@gmail.com>
2023-03-24 20:48:03 -0600

    Added the index template file

--- dinner/urls.py
+++ dinner/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path("app/", views.index, name="dinner.index"),
+]
--- dinner/views.py
+++ dinner/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Recipe
+
 def index(request):
     recipes = Recipe.objects.all()
 
-    return render(request, "/dinner/index.html", {"recipes": recipes})
+    return render(request, "dinner/index.html", {"recipes": recipes})
