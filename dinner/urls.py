88d85f4aeda3f0aa64bc60548157b05d6d602744
Jacob Atkinson <jacobatkinson200@gmail.com>
2023-03-24 22:04:00 -0600

    Fixed the url for real, and added textarea.

--- dinner/urls.py
+++ dinner/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "dinner"
 
 urlpatterns = [
-    path("/", views.index, name="index"),
+    path("", views.index, name="index"),
     path("detail/<int:recipe_id>", views.recipe_detail, name="detail"),
     path("new", views.new_recipe, name="new_recipe"),
     path("create", views.create_recipe, name="create_recipe"),
